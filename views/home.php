99d9e66d49b1d6e68f5a0fbf66463ab39bad8319
milosgs <afegenuim@gmail.com>
2024-04-26 03:39:56 +0100

    this is the second commit already done

--- views/home.php
+++ views/home.php
@@ -12,7 +12,7 @@
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/style.css">
     <link rel="icon" type="image/x-icon" href="assets\image\icare.jpeg">
     <title>icare</title>
 </head>
